c683c9bec217d7e7cbcec7d2321af4d34fb15947
regenerik <regenerik@gmail.com>
2025-06-23 13:17:06 -0300

    nuevo gestor repomatic turbo

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def cargar_usuarios_iniciales():
             "admin": os.getenv(f'ADMIN{i}') == 'True',
             "url_image": os.getenv(f'URL_IMAGE{i}')
         }
-        for i in range(1, 13)  # Del 1 al 12 inclusive
+        for i in range(1, 14)  # Del 1 al 12 inclusive
         if os.getenv(f'EMAIL{i}')  # Solo si hay mail en el .env
     ]
 
